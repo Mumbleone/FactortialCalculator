cac7f800824b9327e759765607de588045d239ac
Student <student@Empowrs-MacBook-Pro.local>
2022-02-08 17:22:12 -0500

    added xcodeproj and changed read me file

---
+++ Factorials/ViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  ViewModel.swift
+//  Factorials
+//
+//  Created by Student on 5/12/21.
+//
+
+import Foundation
+import Combine
+
+class ViewModel: ObservableObject {
+    
+    @Published
+    var viewState: ViewState = .initial
+    
+    enum ViewState {
+        case initial
+        case factorial(Int)
+    }
+    
+    
+    func onCalculateClick(numberString: String?) {
+        
+        guard let factorialString = numberString else {
+            return
+        }
+        
+        let castedFactorialInt = Int(factorialString)
+        
+        guard var int = castedFactorialInt else {
+            return
+        }
+        
+        if int == 0 {
+            return
+        }
+        for i in 1..<int {
+           int *= i
+         }
+       
+        viewState = .factorial(int)
+       
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FactortialCalculator
+
+I created this app to test optionals by using guard statements. This is visible in the ViewModel.swift file. The app calculates factorials.
